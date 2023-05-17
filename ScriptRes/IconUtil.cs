f23465be43b5331cbf50fa2f784ec14a3795820c
Andrii Ihnatiuk <andreyignatuk2002@gmail.com>
2023-05-17 19:49:16 +0300

    v0.2: Can select custom QRes.exe Location, create desktop shortcut, display icon, improved batch script, other

---
+++ ScriptRes/IconUtil.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ScriptRes
+{
+    internal class IconUtil
+    {
+        public Image ImageControl { get; set; }
+        private System.Drawing.Icon? CurrentIcon { get; set; }
+        public string SavedIconPath { get; private set; } = String.Empty;
+
+        public IconUtil(Image control) 
+        { 
+            ImageControl = control;
+        }
+
+        public void DisplayIcon(string iconPath)
+        {
+            CurrentIcon = System.Drawing.Icon.ExtractAssociatedIcon(iconPath);
+
+            if (CurrentIcon != null)
+            {
+                // Convert the icon to a BitmapSource
+                BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                    CurrentIcon.Handle,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+
+                ImageControl.Width = bitmapSource.Width;
+                ImageControl.Height = bitmapSource.Height;
+                ImageControl.Source = bitmapSource;
+            }
+        }
+
+        public bool SaveIcon(string saveLocation)
+        {
+            if (CurrentIcon != null)
+            {
+                if (Directory.Exists(saveLocation))
+                {
+                    string filePath = Path.Combine(saveLocation, Guid.NewGuid().ToString() + ".ico");
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    CurrentIcon.Save(stream);
+
+                    SavedIconPath = filePath;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
--- ScriptRes/MainWindow.xaml.cs
+++ ScriptRes/MainWindow.xaml.cs
@@ -11,9 +11,12 @@ namespace ScriptRes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IconUtil iconUtil;
+
         public MainWindow()
         {
             InitializeComponent();
+            iconUtil = new IconUtil(control: imgIcon);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -23,6 +26,7 @@ namespace ScriptRes
             chBoxQresPath.Unchecked += CheckBox_Checked;
         }
 
+        // Handler for buttons which select file locations
         private void Browse_Click(object sender, RoutedEventArgs e)
         {
             if (sender is not Button btn) return;
@@ -37,17 +41,19 @@ namespace ScriptRes
             }
             if (btn == btnBrowseExec && openFileDialog.ShowDialog() == true)
             {
-                tBoxPath.Text = openFileDialog.FileName;
+                tBoxExecPath.Text = openFileDialog.FileName;
+                iconUtil.DisplayIcon(openFileDialog.FileName);
             }
         }
 
+        // Handler for script generation button
         private void Create_Click(object sender, RoutedEventArgs e)
         {
             string inX = tBoxInX.Text;
             string inY = tBoxInY.Text;
             string outX = tBoxOutX.Text;
             string outY = tBoxOutY.Text;
-            string execPath = tBoxPath.Text;
+            string execPath = tBoxExecPath.Text;
             string qresPath = tBoxQresPath.Text;
 
             if (string.IsNullOrEmpty(inX) || string.IsNullOrEmpty(inY) || string.IsNullOrEmpty(outX) || string.IsNullOrEmpty(outY) || string.IsNullOrEmpty(qresPath))
@@ -55,53 +61,81 @@ namespace ScriptRes
                 return;
             }
 
-            if (!System.IO.Path.Exists(execPath) || !System.IO.Path.IsPathFullyQualified(execPath))
+            if (File.Exists(execPath))
             {
+                // Allow using relative path in program executable location textbox
+                if (Path.IsPathFullyQualified(execPath) == false)
+                {
+                    execPath = Path.GetFullPath(execPath);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Provided program executable doesn't exist or not a file!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
-           
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string precessName = System.IO.Path.GetFileName(execPath);
-            string noExtName = System.IO.Path.GetFileNameWithoutExtension(execPath);
 
-            string script = $"@echo off\r\n" +
-                            $"set processName={precessName}\r\n\r\n" +
-                            $"set inX={inX}\r\n" +
-                            $"set inY={inY}\r\n\r\n" +
-                            $"set outX={outX}\r\n" +
-                            $"set outY={outY}\r\n\r\n" +
-                            "echo Changing the resolution to %outX%x%outY% ...\r\n\"" +
-                            "D:\\QRes\\QRes.exe\" /x:%outX% /y:%outY% > nul\r\n" +
-                            "if not %errorlevel% equ 0 (\r\n" +
+            if (File.Exists(qresPath) && Path.GetFileName(qresPath) == "QRes.exe")
+            {
+                // Allow using relative path in qres location textbox
+                if (Path.IsPathFullyQualified(qresPath) == false)
+                {
+                    qresPath = Path.GetFullPath(qresPath);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Provided QRes.exe location doesn't exist or the file is wrong!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string execName = Path.GetFileName(execPath);
+            string execNameNoExt = Path.GetFileNameWithoutExtension(execPath);
+
+            string script = $"@echo off\r\n\r\n" +
+                            "set exitCode=0\r\n" +
+                            $"echo Changing the resolution to {outX}x{outY} ...\r\n" +
+                            $"\"{qresPath}\" /x:{outX} /y:{outY} | findstr /i \"Error:\" > nul\r\n" +
+                            "if %errorlevel% equ 0 (\r\n" +
+                            "    set exitCode=-1\r\n" +
                             "    goto error\r\n" +
                             ")\r\n" +
                             "echo.\r\n" +
-                            "echo Launching the program \"%processName%\"\r\n" +
+                            $"echo Launching the program \"{execName}\"\r\n" +
                             "echo.\r\n" +
-                            $"{tBoxPath.Text}\r\n\r\n" +
-                            ":loop\r\ntasklist | findstr /i \"%processName%\" > nul\r\n" +
+                            $"\"{execPath}\"\r\n\r\n" +
+                            $":loop\r\ntasklist | findstr /i \"{execName}\" > nul\r\n" +
                             "if not %errorlevel% equ 0 (\r\n" +
                             "    echo Changing resolution back to normal\r\n" +
-                            "    \"D:\\QRes\\QRes.exe\" /x:%inX% /y:%inY% > nul\r\n" +
-                            "    goto end\r\n) \r\n\r\n" +
+                            $"    \"{qresPath}\" /x:{inX} /y:{inY} > nul\r\n" +
+                             "    goto end\r\n) \r\n\r\n" +
                             "timeout /t 2 > nul 2>&1\r\n" +
                             "goto loop\r\n\r\n" +
                             ":error\r\n" +
-                            "echo !!!! Error occured while changing resolution !!!!\r\n\r\n" +
+                            "echo ERROR: Something went wrong, it is likely that the resolution is not supported.\r\n\r\n" +
                             ":end\r\n" +
                             "echo.\r\n" +
-                            "echo Bye\r\n" +
-                            "timeout /t 2 > nul 2>&1";
+                            "echo Finish\r\n" +
+                            "timeout /t 2 > nul 2>&1\r\n" +
+                            "exit /b %exitCode%";
             try
             {
-                File.WriteAllText(desktopPath + $"\\{noExtName}.bat", script);
-                MessageBox.Show($"The file was created: \n{desktopPath}\\{noExtName}.bat");
-            }
-            catch (Exception ex) 
-            {
-                MessageBox.Show(ex.Message);
-            }
+                string batchLocation = Path.Combine(desktopPath, $"{execNameNoExt}.bat");
+                File.WriteAllText(batchLocation, script);
+                string saveLocation = Environment.CurrentDirectory;
+
+                bool iconSaved = iconUtil.SaveIcon(saveLocation);
+                string? iconPath = iconSaved ? iconUtil.SavedIconPath : null; 
                 
+                CreateShortcut(desktopPath, batchLocation, iconPath, execNameNoExt);
+                
+                MessageBox.Show($"The shortcut was created: \n{desktopPath}\\{execNameNoExt}.lnk", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void CheckBox_Checked(object sender, EventArgs e) 
@@ -111,6 +145,28 @@ namespace ScriptRes
             tBoxQresPath.IsEnabled = !box.IsChecked ?? false;
             tBoxQresPath.Text = "./Qres.exe";
             btnChangeQresPath.IsEnabled = !box.IsChecked ?? false;
+        }
+
+        private void CreateShortcut(string shortcutPath, string targetPath, string? iconPath, string name)
+        {
+            var shell = new IWshRuntimeLibrary.WshShell();
+            string location = Path.Combine(shortcutPath, $"{name}.lnk");
+            var shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(location);
+
+            shortcut.TargetPath = targetPath;
+            if (string.IsNullOrEmpty(iconPath))
+            {
+                if (File.Exists("default.ico"))
+                {
+                    shortcut.IconLocation = Path.GetFullPath("default.ico");
+                }
+            }
+            else
+            {
+                shortcut.IconLocation = iconPath;
+            }
+
+            shortcut.Save();
         }
     }
 }
